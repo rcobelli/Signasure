4c01828c55bf6a2060a55ea6e93278aebfaa38fa
Arvin Poddar <arvin.poddar@gmail.com>
2019-10-24 18:53:15 -0400

    Remove multi threading

--- application.py
+++ application.py
@@ -84,4 +84,4 @@ if __name__ == "__main__":
     # removed before deploying a production app.
     application.debug = True
     CORS(application)
-    application.run(threaded=True)
+    application.run()
