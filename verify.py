79631cd79190c11601cf2270b22036ec8248755d
Arvin Poddar <arvin.poddar@gmail.com>
2019-10-24 22:23:15 -0400

    Alter confidence intervals

--- verify.py
+++ verify.py
@@ -18,21 +18,20 @@ def verify(uuid, img_uri):
     fh.write(base64.decodebytes(uri))
     fh.close()
 
-    data = tc.image_analysis.load_images(filename, with_path=True)
+    #data = tc.image_analysis.load_images(filename, with_path=True)
+    test = tc.SFrame({'image': [tc.Image(filename)]})
 
     model = tc.load_model('{}/models/{}.model'.format(sys.path[0], uuid))
     # 3. Generate prediction
-    predictions = model.predict(dataset=data)
-
-    df = pd.DataFrame(predictions, columns=['ouput'])
-
-    print(df)
+    #predictions = model.predict(dataset=data)
+    test['predictions'] = model.predict(test)
+    print(test["predictions"])
 
     os.remove(filename)
 
     return {
         "statusCode": 200,
-        "body": "{}".format(predictions[0])
+        "body": "{}".format(test['predictions'])
     }
 
 
